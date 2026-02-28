772b67937d3c9689efef6cd3e69736832a60ff93
Albreis - Design & Programação <contato@albreis.com.br>
2026-02-28 14:20:55 -0300

    chore: release v2.2.25

--- shorts-api.php
+++ shorts-api.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Shorts API
  * Description: Create a shorts page for videos.
- * Version: 2.2.24
+ * Version: 2.2.25
  * Author: ER Soluções Web
  * Author URI: https://albreis.github.io/shorts-api/
  * Text Domain: shorts-api
